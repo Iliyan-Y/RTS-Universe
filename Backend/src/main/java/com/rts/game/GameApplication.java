ded1e8142eaaa0571e41a7bb51ef0cf8c7832679
Iliyan-Y <maximizebg@gmail.com>
2021-02-24 20:26:57 +0000

    Spring Backend added

---
+++ Backend/src/main/java/com/rts/game/GameApplication.java
@@ -0,0 +1,13 @@
+package com.rts.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/rts/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rts.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
